1408b722e34816d1bba9903405daf9bfd39dd478
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-05-17 03:15:00 +0800

    init(status)

--- Objects/Class-TV.py
+++ Objects/Class-TV.py
@@ -3,6 +3,9 @@ class TV:
     #cursorem(runner)
     def __init__(self, TVchannel):
         self.TVchannel = 1
-        self.TVchannel = TVchannel
-        
+        self.TVchannel = TVchannel 
+    def __init__(self, status):    
+        self.status = False
+        self.status = status
+     
         
