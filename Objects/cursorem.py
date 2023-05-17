dcc5db60e0a3b762f058ac1ec41aa8677192330a
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-05-17 18:34:19 +0800

    Final Code

--- Objects/cursorem.py
+++ Objects/cursorem.py
@@ -10,6 +10,7 @@ TV2 = TV()
 TV2.TV_ON()
 TV2.set_TVchannel(1)
 
-messagebox.showinfo("TELEVISION", "TV1 channel is " + str(TV1.get_TVchannel()) + " and the volume  is " + str(TV1.get_volume()) + "\nTV2 channel is " + str(TV2.get_TVchannel()) + " and the volume  is " + str(TV2.get_volume()))
+messagebox.showinfo("TELEVISION", "TV1 channel is " + str(TV1.get_TVchannel()) + " and the volume  is " + str(TV1.get_volume()) + 
+                    "\nTV2 channel is " + str(TV2.get_TVchannel()) + " and the volume  is " + str(TV2.get_volume()))
 
 
